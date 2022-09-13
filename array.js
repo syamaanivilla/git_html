aabb05c68788d9812afa45812e08d80385819b0d
Girijashyamlamba Anivilla <girija.anivilla@proclink.local>
2022-09-13 17:00:17 +0530

    files for java script

---
+++ array.js
@@ -0,0 +1,6 @@
+var arr=[43,545,554,454,767,345];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[arr.length-2]);
+//methods of an array
+
---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("hiiii");
+// var name="syama";
+// console.log(name);
+// console.log("name");
+// console.log(name+3);
+// var i=5;
+// console.log(i==="5");
+var marks=[53,65,76,89,90]
+// marks=3;
+console.log(marks[0]);
+console.log(marks.length);
+console.log(marks.reverse());
+console.log(marks[marks.length-1]);
+
+
+
